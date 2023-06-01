da7b76cc404cdfa58a09b2340685780047fba004
proximity2081 <135098710+proximity2081@users.noreply.github.com>
2023-06-01 01:42:53 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+var cacheName = "cache-0"
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll([
+        'lex_fridman_podcast_quotes.json',
+        'script.js',
+        'pako.min.js',
+        'style.css',
+        'loading_animation.gif'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
