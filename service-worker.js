21f65f6942560811a9c0205536160327511f03af
proximity2081 <135098710+proximity2081@users.noreply.github.com>
2023-06-03 23:49:21 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,9 +4,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll([
-        'lex_fridman_podcast_quotes.json.gz',
         'script.js',
-        'pako.min.js',
         'style.css',
         'loading_animation.gif'
       ]);
